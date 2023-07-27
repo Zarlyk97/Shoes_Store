e110bc27369ecdc9125672be625ffc0d5a699af6
zarlyk.zhanybekov.bj@gmail.com <you@example.com>
2023-07-28 01:26:55 +0700

    done

--- shoes_store/lib/theme/app_texts.dart
+++ shoes_store/lib/theme/app_texts.dart
@@ -14,4 +14,14 @@ class AppTextStyle {
   static const text = ("Jordan 14 Retro Gym Red \nToro (GS)");
   static const text1 = ("Lowest Ask ");
   static const text2 = ("\$169");
+  static const sizeAll = ("Size All");
+  static const jordan = (" 'Jordan 14 Retro Gym\n Red To',");
+  static const condition = ("Condition:");
+  static const ticker = ("Ticker:JB-JO1RBGRBW");
+  static const authenticc = ("100% Authentic");
+  static const informations = ("Information");
+  static const black = ("BLACK \nWHITE-OFF WHITE-GYM RED");
+  static const related = ("Related Products");
+  static const price = ("Retail Price");
+  static const date = ("Release Date");
 }
--- shoes_store/lib/view/home_view.dart
+++ shoes_store/lib/view/home_view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:shoes_store/constants/inkwell.dart';
-
 import 'package:shoes_store/product.dart';
-import 'package:shoes_store/theme/app_colors.dart';
-
 import 'package:shoes_store/theme/app_texts.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -11,26 +10,28 @@ class MyHomePage extends StatefulWidget {
     super.key,
   });
 
-  //final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // List<String> images = [
-  //   "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/1a6395c2-4b30-4df0-b43e-4575a57b8478/air-max-goaterra-2-mens-boots-5mjdGp.png",
-  //   "https://static.nike.com/a/images/t_default/rorabfpoffxpmekxfaq7/manoa-leather-mens-boots-Mj42BL.png",
-  //   "https://static.nike.com/a/images/t_default/7a39151f-ebd2-4824-9afb-59e52c46a54d/sfb-6-leather-mens-boot-l3Ngv1.png"
-  // ];
+  int _selectedIndex = 0;
+  bool _isPressed = false;
+
+  void _handlePress() {
+    setState(() {
+      _isPressed = !_isPressed;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
+        child: ListView(
+          children: [
+            Column(children: [
               SafeArea(
                 child: Row(
                   children: [
@@ -47,9 +48,16 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ),
                     ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 250, top: 15),
-                      child: Icon(Icons.favorite_border, size: 45),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 250, top: 5),
+                      child: IconButton(
+                        icon: Icon(
+                          _isPressed ? Icons.favorite : Icons.favorite_border,
+                          size: 40,
+                          color: _isPressed ? Colors.red : Colors.black,
+                        ),
+                        onPressed: _handlePress,
+                      ),
                     ),
                   ],
                 ),
@@ -63,9 +71,8 @@ class _MyHomePageState extends State<MyHomePage> {
               const Row(
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text('Size All', style: AppTextStyle.colorSizeAll),
-                  ),
+                      padding: EdgeInsets.only(left: 20),
+                      child: Text(AppTextStyle.sizeAll)),
                   Padding(
                     padding: EdgeInsets.only(left: 250),
                     child: Icon(Icons.keyboard_arrow_down),
@@ -78,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
               const Padding(
                 padding: EdgeInsets.only(right: 88),
                 child: Text(
-                  'Jordan 14 Retro Gym\n Red To',
+                  AppTextStyle.jordan,
                   style: AppTextStyle.jordantext,
                 ),
               ),
@@ -90,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: FittedBox(
                   child: Row(
                     children: [
-                      Text('Condition:'),
+                      Text(AppTextStyle.condition),
                       Text('New', style: AppTextStyle.newtext),
                       SizedBox(width: 5),
                       Padding(
@@ -100,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       SizedBox(
                         width: 5,
                       ),
-                      Text('Ticker:JB-JO1RBGRBW'),
+                      Text(AppTextStyle.ticker),
                       SizedBox(width: 5),
                       Padding(
                         padding: EdgeInsets.only(bottom: 7),
@@ -110,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         width: 5,
                       ),
                       Text(
-                        '100% Authentic',
+                        AppTextStyle.authenticc,
                         style: TextStyle(color: Colors.green),
                       ),
                     ],
@@ -122,7 +129,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               const Padding(
                 padding: EdgeInsets.only(right: 250),
-                child: Text('Information', style: AppTextStyle.information),
+                child: Text(AppTextStyle.informations,
+                    style: AppTextStyle.information),
               ),
               const SizedBox(
                 height: 10,
@@ -150,8 +158,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     SizedBox(
                       width: 43,
                     ),
-                    Text('BLACK \nWHITE-OFF WHITE-GYM RED',
-                        style: AppTextStyle.blackText)
+                    Text(AppTextStyle.black, style: AppTextStyle.blackText)
                   ],
                 ),
               ),
@@ -162,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.only(left: 23),
                 child: Row(
                   children: [
-                    Text('Retail Price'),
+                    Text(AppTextStyle.price),
                     SizedBox(
                       width: 30,
                     ),
@@ -177,7 +184,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.only(left: 23),
                 child: Row(
                   children: [
-                    Text('Release Date'),
+                    Text(AppTextStyle.date),
                     SizedBox(
                       width: 20,
                     ),
@@ -198,110 +205,75 @@ class _MyHomePageState extends State<MyHomePage> {
               const Padding(
                 padding: EdgeInsets.only(right: 200),
                 child: Text(
-                  'Related Products',
+                  AppTextStyle.related,
                   style: AppTextStyle.information,
                 ),
               ),
               const SizedBox(
                 height: 20,
               ),
-              ProductItem2(),
-              // SizedBox(
-              //   width: 360,
-              //   height: 235,
-              //   child: ListView.builder(
-              //     itemCount: 3,
-              //     scrollDirection: Axis.horizontal,
-              //     itemBuilder: (context, index) => ProductItem(
-              //       productName: "Jordan 14 Retro Gym Red \n Toro (GS) $index",
-              //       imageUrl: images[index],
-              //       price: 169 + index,
-              //     ),
-              //   ),
-              // ),
+              const ProductItem2(),
               const SizedBox(
                 height: 30,
               ),
-              Container(
-                height: 80,
-                width: 329,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(50),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withAlpha(80),
-                      blurRadius: 7.0, // soften the shadow
-                      spreadRadius: .0, //extend the shadow
-                      offset: const Offset(
-                        .0, // Move to right 5  horizontally
-                        5.0, // Move to bottom 5 Vertically
-                      ),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 12),
-                      child: Container(
-                        height: 48,
-                        width: 98,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: AppColors.firstcontColor),
-                        child: Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.add_home_outlined,
-                                size: 30,
-                              ),
-                            ),
-                            const Text(
-                              "menu",
-                              style: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: "LilitaOne-Regular"),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 35,
-                    ),
-                    Image.asset(
-                      'assets/image/pngwing.png',
-                      height: 35,
-                      width: 35,
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.search, size: 34),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.person_2_outlined,
-                        size: 34,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 20,
+            ]),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                color: Colors.black.withOpacity(.2),
               )
             ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+              child: GNav(
+                rippleColor: Colors.grey[300]!,
+                hoverColor: Colors.grey[100]!,
+                gap: 8,
+                activeColor: Colors.black,
+                iconSize: 24,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                duration: const Duration(milliseconds: 400),
+                tabBackgroundColor: Colors.grey[100]!,
+                color: Colors.black,
+                tabs: const [
+                  GButton(
+                    icon: Icons.home_outlined,
+                    text: 'menu',
+                  ),
+                  GButton(
+                    icon: Icons.crop_free_outlined,
+                    text: 'Scan',
+                  ),
+                  GButton(
+                    icon: Icons.search,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: Icons.person_outline,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+              ),
+            ),
           ),
         ),
       ),
